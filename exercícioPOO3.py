a9d264ac4830e3b4d25591ce20a9c4a3d30a2751
Joaobento3 <jv3344035@gmail.com>
2025-09-04 14:47:09 -0300

    conclusão do exercício POO3

--- exercícioPOO3.py
+++ exercícioPOO3.py
@@ -3,6 +3,35 @@ class criptografiaTexto:
         self.texto = texto
 
     
-    def criptografando():
+    def criptografando(self):
+        substituicoes = {
+            "a":"4", "A":"4",
+            "e":"3", "E":"3",
+            "i":"1", "I": "1",
+            "o":"0", "O": "0",
+            "u":"8", "U": "8"
+        }
 
-        
+        frase_criptografada = ""
+        for caracter in self.texto:
+            if caracter in substituicoes:
+                frase_criptografada += substituicoes[caracter]
+            else:
+                frase_criptografada += caracter
+        return frase_criptografada
+    
+
+
+def main():
+
+    texto_usuario = input("\nDigite o texto que deseja criptografar: ")
+    criptografador = criptografiaTexto(texto_usuario)
+    resultado = criptografador.criptografando()
+    print(f"\nFrase criptografada: {resultado}\n")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
