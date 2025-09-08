ac8ee312b64614278c2c91a1cb451424496dcb2e
Joaobento3 <jv3344035@gmail.com>
2025-09-08 16:54:34 -0300

    criação do  arquivo exercícioPOO6.py

---
+++ exercícioPOO6.py
@@ -0,0 +1,39 @@
+
+
+
+
+def main():
+    funcionarios = []
+    departamentos = []
+
+
+    while True:
+        print("\n===MENU===")
+        print("1. Criar funcionário")
+        print("2. Criar departamento")
+        print("3. Adicionar funcionário ao departamento")
+        print("4. Listar funcionários de um departamento")
+        print("5. Mostrar média salarial do departamento")
+        print("6. Sair")
+
+        opcao = input("\nEscolha uma opção: ")
+
+        if opcao == "1":
+
+
+        elif opcao == "2":
+
+
+        elif opcao == "3":
+
+
+        elif opcao == "4":
+
+
+        elif opcao == "5":
+
+
+        elif opcao == "6":
+
+
+        
